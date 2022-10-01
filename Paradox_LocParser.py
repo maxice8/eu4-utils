ada1968ee893d0e0228e62af4e19ffeed40c96ae
maxice8 <thinkabit.ukim@gmail.com>
2022-10-01 15:10:30 -0300

    make ParadoxLocalisation{Lexer,Parser} more robust

--- Paradox_LocParser.py
+++ Paradox_LocParser.py
@@ -11,21 +11,25 @@ class ParadoxLocalisationLexer(Lexer):
     }
 
     literals = {":"}
-    ignore = " \t"
-
-    @_(r"\-?\d+")
-    def INTEGER(self, t):
-        t.value = int(t.value)
-        return t
+    # \ufeff is the Byte-Order-Mark that is at the start of every
+    # localisation file, we choose to ignore it outright as it does
+    # not affect anything
+    ignore = " \t\ufeff"
 
     # Anything inside doule-quotes
     STRING = r'".*"'
 
+    @_(r"\b[0-9]+\b")
+    def INTEGER(self, t):
+        t.value = int(t.value)
+        return t
+
     # Something like localisation_key
-    KEY = r"[a-zA-Z][^ :]*"
+    KEY = r"[A-Za-z0-9\_\-\.]+"
 
     # Line number tracking
-    @_(r"\n")
+    # Also account for \r\n from Windows
+    @_(r"\n", r"\r\n")
     def ignore_newline(self, t):
         self.lineno += t.value.count("\n")
 
@@ -33,10 +37,11 @@ class ParadoxLocalisationLexer(Lexer):
     ignore_comment = r"\#.*"
 
     def error(self, t):
-        print("Illegal Character '%s'" % t.value[0])
+        print("Line %d: Bad character %r" % (self.lineno, t.value[0]))
         self.index += 1
 
 
+# TODO: make so this returns a single shared dict with different keys, in the meantime we will have to deal with compressing it ourselves.
 class ParadoxLocalisationParser(Parser):
     tokens = ParadoxLocalisationLexer.tokens
 

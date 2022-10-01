6a5d3dc98780f58253d8677b31a938a92301db0c
maxice8 <thinkabit.ukim@gmail.com>
2022-10-01 15:14:18 -0300

    move generate_localisation to Paradox_LocParser

--- Generate-Policy-Table.py
+++ Generate-Policy-Table.py
@@ -82,64 +82,6 @@ def read_all_files_in_dir(dir: str) -> str:
     return wfd.getvalue()
 
 
-def generate_localisation(dir: str, base: str = None) -> dict[str, dict[str, Any]]:
-    """Generate a dictionary of dictionary representing localisation keys
-
-    Args:
-        dir (str): directory of the mod to get localisation
-        base (str, optional): base game to get localisation as a backup. Defaults to None.
-
-    Returns:
-        dict[str, dict[str, Any]]: A dictionary of dictionaries representing localisation keys
-    """
-
-    from Paradox_LocParser import ParadoxLocalisationLexer, ParadoxLocalisationParser
-
-    localisation: dict[str, dict[str, Any]] = dict()
-
-    # Will hold ALL the files that we are going to parse
-    wfd = io.StringIO()
-
-    # Get all the base games ones if given to us
-    if base is not None:
-        # For some reason args.base is passed as a [list] but it only
-        # receives a single argument
-        for file in os.listdir(base[0] + "/localisation"):
-            if not file.endswith("l_english.yml"):
-                continue
-            with open(
-                f"{base[0]}/localisation/{file}", "r", encoding="utf-8-sig"
-            ) as fd:
-                wfd.write((fd.read()))
-                wfd.write("\n")
-
-    # Get all the modding ones after as they are the ones that will
-    # end up composing the final dictionary, I really should make the
-    # parser work by having a single dictionary.
-    for file in os.listdir(dir + "/localisation"):
-        if not file.endswith("l_english.yml"):
-            continue
-        with open(f"{dir}/localisation/{file}", "r", encoding="utf-8-sig") as fd:
-            wfd.write(fd.read())
-            wfd.write("\n")
-
-    stream = wfd.getvalue()
-
-    # Lex and Parse
-    lexer = ParadoxLocalisationLexer()
-    parser = ParadoxLocalisationParser()
-    result = parser.parse(lexer.tokenize(stream))
-
-    # Create our new dictionary
-    for res in result:
-        if type(res) is not dict:
-            continue
-
-        localisation[res["key"]] = {"value": res["value"], "version": res["version"]}
-
-    return localisation
-
-
 def generate_policy_list(dir) -> dict[str, list[str]]:
     """Generates a dictionary of defined policies returning a dictionary with all policies and what ideas one must have. Assumes ideas found via potential = { has_idea_group = <idea> } are the only requirements
 
@@ -301,6 +243,10 @@ def main(args=None) -> int:
 
     # Localise
     if args.localise:
+        # Helper function to generate dictionary mapping keys
+        # to values and versions
+        from Paradox_LocParser import generate_localisation
+
         localisation = generate_localisation(moddir, base=args.base)
 
         # Run over all the values of the table and replace them.
--- Paradox_LocParser.py
+++ Paradox_LocParser.py
@@ -1,5 +1,6 @@
 # Paradox_LocParser
 
+from typing import Any
 from sly import Lexer, Parser
 
 
@@ -80,3 +81,62 @@ class ParadoxLocalisationParser(Parser):
     @_("STRING")
     def value(self, p):
         return p[0]
+
+
+def generate_localisation(dir: str, base: str = None) -> dict[str, dict[str, Any]]:
+    """Generate a dictionary of dictionary representing localisation keys
+
+    Args:
+        dir (str): directory of the mod to get localisation
+        base (str, optional): base game to get localisation as a backup. Defaults to None.
+
+    Returns:
+        dict[str, dict[str, Any]]: A dictionary of dictionaries representing localisation keys
+    """
+
+    import io
+    import os
+
+    localisation: dict[str, dict[str, Any]] = dict()
+
+    # Will hold ALL the files that we are going to parse
+    wfd = io.StringIO()
+
+    # Get all the base games ones if given to us
+    if base is not None:
+        # For some reason args.base is passed as a [list] but it only
+        # receives a single argument
+        for file in os.listdir(base[0] + "/localisation"):
+            if not file.endswith("l_english.yml"):
+                continue
+            with open(
+                f"{base[0]}/localisation/{file}", "r", encoding="utf-8-sig"
+            ) as fd:
+                wfd.write((fd.read()))
+                wfd.write("\n")
+
+    # Get all the modding ones after as they are the ones that will
+    # end up composing the final dictionary, I really should make the
+    # parser work by having a single dictionary.
+    for file in os.listdir(dir + "/localisation"):
+        if not file.endswith("l_english.yml"):
+            continue
+        with open(f"{dir}/localisation/{file}", "r", encoding="utf-8-sig") as fd:
+            wfd.write(fd.read())
+            wfd.write("\n")
+
+    stream = wfd.getvalue()
+
+    # Lex and Parse
+    lexer = ParadoxLocalisationLexer()
+    parser = ParadoxLocalisationParser()
+    result = parser.parse(lexer.tokenize(stream))
+
+    # Create our new dictionary
+    for res in result:
+        if type(res) is not dict:
+            continue
+
+        localisation[res["key"]] = {"value": res["value"], "version": res["version"]}
+
+    return localisation

4daefb88a3369035f8e7bf257cbdc62513492478
maxice8 <thinkabit.ukim@gmail.com>
2022-10-03 13:50:13 -0300

    add language support

--- Paradox_Localisation/utils.py
+++ Paradox_Localisation/utils.py
@@ -3,16 +3,17 @@ from Paradox_Localisation.lexer import LocalisationLexer
 from Paradox_Localisation.parser import LocalisationParser
 
 
-def generate_localisation(dirs: list[str]) -> dict[str, dict[str, Any]]:
-    """Generate a dictionary of dictionary representing localisation keys
+def generate_localisation(
+    dirs: list[str], language: str = "english"
+) -> dict[str, dict[str, Any]]:
+    """generate localisation from list of directories.
 
     Args:
-        dir (str): directory of the mod to get localisation
-        base (str, list[str], optional): collection of directories to load localisation from, directories are read in reverse given order so you can pass the base game first then any mods in the loading order. Defaults to None.
+        dirs (list[str]): list of paths to directories where we get load all the localisation files.
+        language (str, optional): which language do we get our localisation for. Defaults to "english".
 
     Returns:
-        dict[str, dict[str, Any]]: A dictionary of dictionaries
-        representing localisation keys
+        dict[str, dict[str, Any]]: a dictionary of dictionaries keyed by the localisation key and holding the value of the localisation and its version.
     """
 
     import io
@@ -30,7 +31,7 @@ def generate_localisation(dirs: list[str]) -> dict[str, dict[str, Any]]:
     # Get all the base games ones if given to us
     for dir in reversed(dirs):
         for file in os.listdir(dir + "/localisation"):
-            if not file.endswith("l_english.yml") or file in files_seen:
+            if not file.endswith(f"l_{language}.yml") or file in files_seen:
                 continue
             with open(f"{dir}/localisation/{file}", "r", encoding="utf-8-sig") as fd:
                 wfd.write((fd.read()))

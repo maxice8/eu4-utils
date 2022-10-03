1329ef30ba8a70860b44d21fffef18137e3c1113
maxice8 <thinkabit.ukim@gmail.com>
2022-10-03 18:27:36 -0300

    add has_mapping util

--- hw_utils/__init__.py
+++ hw_utils/__init__.py
@@ -1,3 +1,4 @@
 from .markdown import make_markdown_table
+from .walk import has_mapping
 
-__all__ = "make_markdown_table"
+__all__ = ("make_markdown_table", "has_mapping")
---
+++ hw_utils/walk.py
@@ -0,0 +1,25 @@
+from operator import truediv
+
+
+def has_mapping(tree: tuple, mapping: tuple, in_not: bool = False) -> bool:
+    """check if a tuple is present in the given tree
+
+    Args:
+        tree (tuple): the tree to be checked
+        mapping (tuple): the tuple that needs to be checked, example: ("tag", "FRA")
+        in_not (bool, optional): whether we are inside a NOT = { }, which negates any matches and is used internally. Defaults to False.
+
+    Returns:
+        bool: whether the mapping is present in the tree
+    """
+    for elem in tree:
+        if elem[0].upper() == "NOT":
+            if has_mapping(elem[1], mapping, in_not=not in_not):
+                return True
+        elif any(x == elem[0].upper() for x in ["AND", "OR"]):
+            if has_mapping(elem[1], mapping, in_not=not in_not):
+                return True
+        elif elem == mapping and in_not is False:
+            return True
+
+    return False

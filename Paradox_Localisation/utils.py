6dfa5377aeb3f8c954fd84499f2af871def05da3
maxice8 <thinkabit.ukim@gmail.com>
2022-10-03 12:18:57 -0300

    generate_localisation: simplify usage

--- Paradox_Localisation/utils.py
+++ Paradox_Localisation/utils.py
@@ -3,9 +3,7 @@ from Paradox_Localisation.lexer import LocalisationLexer
 from Paradox_Localisation.parser import LocalisationParser
 
 
-def generate_localisation(
-    basedir: str, extra_dirs: str | list[str] | None = None
-) -> dict[str, dict[str, Any]]:
+def generate_localisation(dirs: list[str]) -> dict[str, dict[str, Any]]:
     """Generate a dictionary of dictionary representing localisation keys
 
     Args:
@@ -29,40 +27,17 @@ def generate_localisation(
     # Will hold ALL the files that we are going to parse
     wfd = io.StringIO()
 
-    # Get all the modding ones after as they are the ones that will
-    # end up composing the final dictionary, I really should make the
-    # parser work by having a single dictionary.
-    for file in os.listdir(basedir + "/localisation"):
-        if not file.endswith("l_english.yml"):
-            continue
-        # open with utf-8-sig because of the Byte-Order-Mark required
-        # in localisation files
-        with open(f"{basedir}/localisation/{file}", "r", encoding="utf-8-sig") as fd:
-            wfd.write(fd.read())
-            wfd.write("\n")
-
-        # Don't bother checking if in set, this is the first time
-        files_seen.add(file)
-
     # Get all the base games ones if given to us
-    if extra_dirs is not None:
-        if type(extra_dirs) is str:
-            # Redeclare as a list
-            extra_dirs = [extra_dirs]
-        # For some reason args.base is passed as a [list] but it only
-        # receives a single argument
-        for dir in reversed(extra_dirs):
-            for file in os.listdir(dir + "/localisation"):
-                if not file.endswith("l_english.yml") or file in files_seen:
-                    continue
-                with open(
-                    f"{dir}/localisation/{file}", "r", encoding="utf-8-sig"
-                ) as fd:
-                    wfd.write((fd.read()))
-                    wfd.write("\n")
+    for dir in reversed(dirs):
+        for file in os.listdir(dir + "/localisation"):
+            if not file.endswith("l_english.yml") or file in files_seen:
+                continue
+            with open(f"{dir}/localisation/{file}", "r", encoding="utf-8-sig") as fd:
+                wfd.write((fd.read()))
+                wfd.write("\n")
 
-                # Say we have seen the file
-                files_seen.add(file)
+            # Say we have seen the file
+            files_seen.add(file)
 
     stream = wfd.getvalue()
 

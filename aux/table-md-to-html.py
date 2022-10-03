f4b4aebbbe056b13f92bf2b85944089b4785a435
maxice8 <thinkabit.ukim@gmail.com>
2022-10-03 12:45:36 -0300

    aux/*: update

--- aux/table-md-to-html.py
+++ aux/table-md-to-html.py
@@ -20,6 +20,8 @@ TEMPLATE = """<!DOCTYPE html>
             border-collapse: collapse;
             border-spacing: 0;
             font: normal 13px Arial, sans-serif;
+            margin-left: auto;
+            margin-right: auto;
         }
         table thead th {
             background-color: #DDEFEF;
@@ -29,15 +31,7 @@ TEMPLATE = """<!DOCTYPE html>
             text-align: left;
             text-shadow: 1px 1px 1px #fff;
         }
-        table tbody tr td:first-child {
-            background-color: #DDEFEF;
-            border: solid 1px #DDEEEE;
-            color: #336B6B;
-            padding: 10px;
-            text-align: left;
-            text-shadow: 1px 1px 1px #fff;
-            font-weight: bold
-        }
+        {{first_child}}
         table tbody td {
             border: solid 1px #DDEEEE;
             color: #333;
@@ -72,6 +66,12 @@ def parse_args(args=None):
         default=sys.stdout,
         help="Output file name. Defaults to stdout.",
     )
+    parser.add_argument(
+        "--highlight-first-row",
+        default=False,
+        action="store_true",
+        help="Highlight the first row on the left",
+    )
     return parser.parse_args(args)
 
 
@@ -80,7 +80,21 @@ def main(args=None):
     md = args.mdfile.read()
     extensions = ["extra", "smarty"]
     html = markdown.markdown(md, extensions=extensions, output_format="html5")
-    doc = jinja2.Template(TEMPLATE).render(content=html)
+    if args.highlight_first_row:
+        first_child = """
+        table tbody tr td:first-child {
+            background-color: #DDEFEF;
+            border: solid 1px #DDEEEE;
+            color: #336B6B;
+            padding: 10px;
+            text-align: left;
+            text-shadow: 1px 1px 1px #fff;
+            font-weight: bold
+        }
+        """
+    else:
+        first_child = ""
+    doc = jinja2.Template(TEMPLATE).render(content=html, first_child=first_child)
     args.out.write(doc)
 
 
